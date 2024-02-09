a43bc52cf431883c621e5b5d54f0c029f8cc7cce
George Gatheca <gatheca.george@gmail.com>
2024-02-09 11:50:50 +0000

    run and build commands

--- media.sh
+++ media.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+docker build -f Dockerfile.cinnamon -t ubuntu-vm .
 docker run -p 3389:3389 -p 5902:5902 -p 8096:8096 -d --name ubuntu-vm -ti ubuntu-vm
+
+docker build -f Dockerfile.cinnamon -t ubuntu-vm2 .
+docker run -p 3390:3389 -p 5903:5902 -p 8097:8096 -d --name ubuntu-vm2 -ti ubuntu-vm2
